eba36402bfa20e881a0d5444d20fc38d903a5211
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-04-16 09:34:16 -0400

    sdasd

--- client/src/Create.jsx
+++ client/src/Create.jsx
@@ -3,7 +3,7 @@ import axios from 'axios' //library for making http reuquests, and it is used to
 
 function Create() {
 
-  const [task, setTask] = useState
+  const [task, setTask] = useState();
 
   const handleAdd=()=>{ //axios post request to localhost when add button is clicked
     axios.post('http://localhost:3001/add', {task:task}) //Only the code within the .then/.catch waits for the api call, otherwise everything is run chronologically
@@ -12,8 +12,8 @@ function Create() {
   } 
   return (
     <div className='flex justify-center pt-4' >
-      <input className='rounded-lg w-1/6 h-8' placeholder='Enter Task' onChange={(e)=> setTask(e.target.value)}></input>
-      <button className='rounded-lg bg-violet-700 w-16' type='button' onClick={handleAdd}>Add</button>
+      <input className='rounded-lg w-1/6 h-8 pl-2' placeholder='Enter Task' onChange={(e)=> setTask(e.target.value)}></input>
+      <button className='ml-1 rounded-lg bg-violet-700 w-16' type='button' onClick={handleAdd}>Add</button>
     </div>
   )
 }
